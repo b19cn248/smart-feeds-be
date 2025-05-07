9d277c61f771ee69add11534785cf51e177d5f74
hieunguyen <hieunguyen@vnpt.vn>
2025-05-07 21:57:11 +0700

    test jenkins work

--- feeds-api/src/main/java/com/olh/feeds/api/controller/TestController.java
+++ feeds-api/src/main/java/com/olh/feeds/api/controller/TestController.java
@@ -13,4 +13,9 @@ public class TestController {
         return "Server is running";
     }
 
+    // Test API to check if the application is up and running
+    @RequestMapping("/status")
+    public String statusCheck() {
+        return "Application is up and running";
+    }
 }
